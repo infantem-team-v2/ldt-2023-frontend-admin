0a413be7971736a864252521f5ab441adff6d4f7
kirshik <dhdjsjsdj@gmail.com>
2023-05-28 23:18:38 +0300

    ff

--- src/components/ConsolePage.js
+++ src/components/ConsolePage.js
@@ -1,12 +1,15 @@
 
-import { Navigate } from "react-router-dom";
 import Constants from "./Constants";
 const ConsolePage = ({ auth }) => {
 
   console.log(auth);
   return (
     <>{
-      auth ? <Constants /> : <Navigate to="/auth" />
+      auth ?
+        <div>
+          < h1 > Console Page</h1 >
+          <Constants />
+        </div > : <Navigate to="/auth" />
 
     }
     </>
