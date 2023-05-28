c3cdbac1bd97a2855c6d6efeb89b47e68a55ed67
kirshik <dhdjsjsdj@gmail.com>
2023-05-28 22:09:51 +0300

    ff

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../services/api";
 import { nanoid } from "nanoid";
-import { Collapse } from "bootstrap";
+import { Collapse } from "react-bootstrap";
 
 
 
