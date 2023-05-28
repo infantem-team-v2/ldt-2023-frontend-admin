05de29b6467e49411a8d40003979e4e696c57bd1
kirshik <dhdjsjsdj@gmail.com>
2023-05-28 21:23:12 +0300

    ff

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
 import './App.css';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import AdminForm from './components/AdminForm';
 import { useState, useEffect } from 'react';
 import api from './services/api';
 import ConsolePage from './components/ConsolePage';
 
-const PrivateRoute = ({ auth: { isAuthenticated }, children }) => {
-  return isAuthenticated ? children : <Navigate to="/auth" />;
-};
+// const PrivateRoute = ({ auth: { isAuthenticated }, children }) => {
+//   return isAuthenticated ? children : <Navigate to="/auth" />;
+// };
 
 function App() {
 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    fetchLoggedIn()
-  }, []);
-
-  const fetchLoggedIn = async () => {
-    try {
-      const res = await api.get("/auth/check");
+    api.get("/auth/check").then(res => {
       if (res.status >= 200 && res.status < 300) {
         setIsAuthenticated(true);
       }
-    } catch (err) {
+    }
+    ).catch(err => {
       setIsAuthenticated(false);
     }
-  }
+    )
+  }, [isAuthenticated]);
+
+
   return (
     <>
       <Routes>
         <Route path='/auth' element={<AdminForm setIsAuthenticated={setIsAuthenticated} />} />
-        <Route path='/' element={<PrivateRoute auth={isAuthenticated}> <ConsolePage /></PrivateRoute>} />
+        <Route path='/' element={<ConsolePage />} />
       </Routes>
     </>
   );
